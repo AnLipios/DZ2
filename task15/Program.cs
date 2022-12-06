2232b649a1ed25c5ac022936f1d600db312459d2
Anna <ogonek0918@yandex.ru>
2022-12-06 17:46:17 +0700

    version

--- task15/Program.cs
+++ task15/Program.cs
@@ -5,19 +5,14 @@
 Console.Clear();
 Console.Write("Введите номер дня недели: ");
 int number = int.Parse(Console.ReadLine());
-while (number<6)
+if ((number<6) && (number >0))
 {
     
     Console.WriteLine("Иди работать");
-    number++;
 }
-if (number == 6||number == 7)
+if ((number == 6)||(number == 7))
 
 {
     Console.WriteLine("Ура! Это выходной!");
-}
-else 
-{
-    Console.WriteLine("Такого дня недели нет");
 }
 
