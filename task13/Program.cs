f6eec7633fd11da2bd0735e59fbefde0d3ba151c
Anna <ogonek0918@yandex.ru>
2022-12-06 17:29:13 +0700

    rename

---
+++ task13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит 
+//третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нету");
+}
+else
+{
+    while (number >= 1000)
+    {
+        number = number / 10;
+
+    }
+    number = number%10;
+    Console.WriteLine($"Третья цифра - {number}");
+}
